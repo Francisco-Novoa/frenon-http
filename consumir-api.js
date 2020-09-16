daf93b780090158f1eac6442e72772fcb4346d27
Francisco-Novoa <francisco.novoa.14@sansano.usm.cl>
2020-09-16 12:19:39 -0300

    chore:get titles now returns array of titles

--- consumir-api.js
+++ consumir-api.js
@@ -1,61 +1,60 @@
+const https = require("https");
 
-
-const https = require('https');
-
-const doRequest= (options, data) => {
+const doRequest = (options, data) => {
   return new Promise((resolve, reject) => {
     const req = https.request(options, (res) => {
-      console.log('STATUS: ' + res.statusCode);
-      console.log('HEADERS: ' + JSON.stringify(res.headers));
-      res.setEncoding('utf8');
-      let responseBody = '';
+      console.log("STATUS: " + res.statusCode);
+      console.log("HEADERS: " + JSON.stringify(res.headers));
+      res.setEncoding("utf8");
+      let responseBody = "";
 
-      res.on('data', (chunk) => {
+      res.on("data", (chunk) => {
         responseBody += chunk;
       });
 
-      res.on('end', () => {
+      res.on("end", () => {
         resolve(JSON.parse(responseBody));
       });
     });
 
-    req.on('error', (err) => {
+    req.on("error", (err) => {
       reject(err);
     });
     req.end();
   });
-}
+};
 
-const getApi = async (substr)=>{
-    try {
-        const data = JSON.stringify({});
-        const options = {
-            hostname: 'jsonmock.hackerrank.com',
-            port: 443,
-            path: `/api/movies/search/?Title=${substr}`,
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        };
-        return await doRequest(options, data);
-    }catch(err) {
-        return err
-    }    
-    
-}
+const getApi = async (substr) => {
+  try {
+    const data = JSON.stringify({});
+    const options = {
+      hostname: "jsonmock.hackerrank.com",
+      port: 443,
+      path: `/api/movies/search/?Title=${substr}`,
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    return await doRequest(options, data);
+  } catch (err) {
+    return err;
+  }
+};
 
 const getMovieTitles = async (substr) => {
-    try{
-        const uri = `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}`;
-        const getdata = await getApi(substr);
-        console.log('--getdata--', getdata);
-        return getdata;
-
-    } catch (error) {
+  try {
+    const uri = `https://jsonmock.hackerrank.com/api/movies/search/?Title=${substr}`;
+    const getdata = await getApi(substr);
+    console.log("--getdata--", getdata);
+    return getdata;
+  } catch (error) {
     return error;
-    }
-}
+  }
+};
 
-const titles = getMovieTitles('spiderman')
-.then( (response)=> console.log('--response--', response) )
+const titles = getMovieTitles("spiderman").then((response) =>
+  response.data.map((movie) => movie.Title).sort()
+);
+
+console.log(titles);
